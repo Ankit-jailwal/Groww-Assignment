747e82205c8f5a57337b3ff9a2a505d6417347c2
Ankit-jailwal <jailwalankit@gmail.com>
2022-11-10 17:43:12 +0530

    Created routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Feed from './Pages/Feed/Feed';
+import Profile from './Pages/Profile/Profile';
+import { Routes, Route, Navigate, HashRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Routes>        
+          <Route exact path='/feed' element={<Feed />} />
+          <Route exact path='/profile' element={<Profile />} />
+          <Route path="*" element={<Navigate replace to="/Feed" />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
-}
+} 
 
 export default App;
