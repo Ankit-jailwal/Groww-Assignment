b6605ee8e68a5191213ba82d9c94c6a3538c6003
Ankit-jailwal <jailwalankit@gmail.com>
2022-11-11 15:37:49 +0530

    Added card in feed

--- src/Pages/Feed/Feed.js
+++ src/Pages/Feed/Feed.js
@@ -1,9 +1,69 @@
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import ListView from "../../Components/ListView/ListView";
 import "./Feed.css";
+import { fetchImages, nextPage, removeError } from "../../Service/Utils";
+import InfiniteScroll from 'react-infinite-scroller';
+
 
 function Feed() {
-    return (
-        <div className="feed">
+    const state = useSelector((state) => state.feedData);
+    const dispatch = useDispatch();
 
+    const [noData, setNoData] = useState(false);
+    const [errorOccured, setErrorOccured] = useState(false);
+
+    useEffect(() => {
+        // fetch images when component is loaded initially
+        dispatch(fetchImages());
+
+        // remove error while closing component
+        return () => dispatch(removeError());
+    }, [])
+
+    useEffect(() => {
+        if (state.imageList.length === 0) {
+            setNoData(true);
+        }
+        else {
+            setNoData(false);
+            dispatch(removeError());
+        }
+
+        if (state.error !== '') {
+            setErrorOccured(true);
+        }
+        else {
+            setErrorOccured(false);
+        }
+
+    }, [state])
+
+    function loadMore() {
+        dispatch(fetchImages()); 
+        dispatch(nextPage());
+    }
+
+    return (
+        <div className="feed flex justify_content_center">
+            <div className="feed_container">
+                {
+                    errorOccured && (
+                        <div className="feed_error">
+                            <pre>
+                                {JSON.stringify(state.error)}!!!
+                            </pre>
+                        </div>
+                    )
+                }
+                {
+                    !noData && (
+                        
+                            <ListView data={state?.imageList}/>
+
+                    )
+                }
+            </div>
         </div>
     );
 }
